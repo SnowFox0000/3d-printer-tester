b6641fa140bc0678bc184e42112d7fe7f7760193
Harry Quan <harryquanx@gmail.com>
2025-05-30 09:17:13 +1200

    re arrange simple script into function

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 from pprint import pprint
 import json 
 import config as cf
+
 """
 Introduction: this is a script to find the average error of the a 3d printer to the measurment of the design
 """
 
-#assigning a catergory to the data
-data = {
-	"height": cf.height, 
-	"width": cf.width, 
-	"inner_diameter": cf.inner_diameter, 
-	"thickness": cf.thickness}
+def search_print_size_error(data: dict):
+	#extract all key in dict data for illiteration, the keys are used for selecting data
+	#and print result with a catergory 
+	keywords = list(data.keys())
 
-#extract all key in dict for illiteration, the keys are used for selecting data
-#and print result with a catergory 
-keywords = list(data.keys())
+	f = 4
+	for k in keywords:
+		d = data[k]
 
-f = 4
-for k in keywords:
-	d = data[k]
+		diff = []
+		for i in d:
+			default = i[0]
+			min_diff = abs(round(default - min(i[1]), f))
+			max_diff = abs(round(default - max(i[1]),  f))
+			# print(min_diff, max_diff)
+			diff.append([min_diff, max_diff])
 
-	diff = []
-	for i in d:
-		default = i[0]
-		min_diff = abs(round(default - min(i[1]), f))
-		max_diff = abs(round(default - max(i[1]),  f))
-		# print(min_diff, max_diff)
-		diff.append([min_diff, max_diff])
+		# print("diff", diff)
+		average_diff = []
+		
+		min_diff = [min(i) for i in diff]
+		# print("min diff", min_diff)
+		average_min_diff = sum(min_diff)/len(min_diff)
 
-	# print("diff", diff)
-	average_diff = []
-	
-	min_diff = [min(i) for i in diff]
-	# print("min diff", min_diff)
-	average_min_diff = sum(min_diff)/len(min_diff)
+		max_diff = [max(i) for i in diff]
+		average_max_diff = sum(max_diff)/len(max_diff)
+		average_diff.append([average_min_diff, average_max_diff])
 
-	max_diff = [max(i) for i in diff]
-	average_max_diff = sum(max_diff)/len(max_diff)
-	average_diff.append([average_min_diff, average_max_diff])
+		# print(min_diff)
+		# print(max_diff)
+		# print(f"{k}")
 
-	# print(min_diff)
-	# print(max_diff)
-	# print(f"{k}")
+		result = {"category": k,
+				  "min_diff:": min(min_diff),
+				  "max_diff": max(max_diff),
+				  "average_min_diff": average_min_diff,
+				  "average_max_diff": average_max_diff,
+				}
 
-	result = {"category": k,
-			  "min_diff:": min(min_diff),
-			  "max_diff": max(max_diff),
-			  "average_min_diff": average_min_diff,
-			  "average_max_diff": average_max_diff,
-			}
+		print(json.dumps(result, indent=4))
 
-	print(json.dumps(result, indent=4))
+if __name__ == "__main__":
+	#assigning a catergory to the data
+	data = {
+		"height": cf.height, 
+		"width": cf.width, 
+		"inner_diameter": cf.inner_diameter, 
+		"thickness": cf.thickness}
+
+	search_print_size_error(data)
